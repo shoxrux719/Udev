099f9c9a50c429d6fe0bd16350d9704c052f9225
ibrohim-001 <rustamovibrohimjon62@hmail.com>
2025-02-03 20:00:52 +0500

    ref: App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Route, Routes } from 'react-router-dom'
-import '@fontsource/poppins/700.css'; // Жирный стиль (700)
-import Home from './_root/pages/Home'
-import RootLayout from './_root/RootLayout'
+import { Route, Routes } from "react-router-dom";
+import "@fontsource/poppins/700.css"; // Жирный стиль (700)
+import Home from "./_root/pages/Home";
+import RootLayout from "./_root/RootLayout";
 
 function App() {
   return (
     <Routes>
-      
       <Route element={<RootLayout />}>
         <Route index element={<Home />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
