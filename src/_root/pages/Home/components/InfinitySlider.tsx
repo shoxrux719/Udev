23b9b511334cf9b61e6b4c1eadff92fc8d4c7d11
ibrohim-001 <rustamovibrohimjon62@hmail.com>
2025-02-09 13:06:47 +0500

    feat:sider is ready

--- src/_root/pages/Home/components/InfinitySlider.tsx
+++ src/_root/pages/Home/components/InfinitySlider.tsx
@@ -2,33 +2,61 @@ import { InfiniteSlider } from "@/components/ui/infinite-slider";
 
 export function InfiniteSliderBasic() {
   return (
-    <InfiniteSlider gap={108} reverse>
-      <img
-        src="/public/assets/images/sliderLogos/image 313 (1).png"
-        alt="Strava logo"
-        className="h-[64px] w-[318px]"
-      />
-      <img
-        src="/public/assets/images/sliderLogos/image.png"
-        alt="Chrome logo"
-        className="h-[62px] w-[122px]"
-      />
-      <img
-        src="/public/assets/images/sliderLogos/uzumMarket.png"
-        alt="Nintendo logo"
-        className="h-[64px] w-[205px]"
-      />
-      <img
-        src="/public/assets/images/sliderLogos/image 317.png"
-        alt="Nintendo logo"
-        className="h-[64px] w-[205px]"
-      />{" "}
-      <img
-        src="/public/assets/images/sliderLogos/uzumMarket.png"
-        alt="Nintendo logo"
-        className="h-[64px] w-[205px]"
-      />
-    </InfiniteSlider>
+    <div className="flex flex-col items-center mt-[148px]">
+      <p className="font-bold text-[40px] leading-[48px]">
+        Bitiruvchilarimiz ish joylari{" "}
+      </p>
+
+      <InfiniteSlider gap={108} reverse={false} className="mt-[49px]">
+        <img
+          src="/public/assets/images/sliderLogos/image 313 (1).png"
+          alt="Strava logo"
+          className="h-[64px] w-[318px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/image.png"
+          alt="Chrome logo"
+          className="h-[62px] w-[122px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/uzummarket.png"
+          alt="Nintendo logo"
+          className="h-[64px] w-[205px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/image 317.png"
+          alt="Nintendo logo"
+          className="h-[93px] w-[189px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/image 318.png"
+          alt="Nintendo logo"
+          className="h-[76px] w-[132px]"
+        />
+      </InfiniteSlider>
+      <InfiniteSlider gap={108} reverse className="mt-[49px]">
+        <img
+          src="/public/assets/images/sliderLogos/image 313 (1).png"
+          alt="Strava logo"
+          className="h-[64px] w-[318px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/image.png"
+          alt="Chrome logo"
+          className="h-[62px] w-[122px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/image 317.png"
+          alt="Nintendo logo"
+          className="h-[93px] w-[189px]"
+        />
+        <img
+          src="/public/assets/images/sliderLogos/image 318.png"
+          alt="Nintendo logo"
+          className="h-[76px] w-[132px]"
+        />
+      </InfiniteSlider>
+    </div>
   );
 }
 
